01391af686aa23fa1142e8e2b8110ef84f6ebc64
Luiz Felipe Xavier <luizfelipexavier@hotmail.com>
2024-01-27 09:27:02 -0300

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,13 +1,13 @@
 export const environment = {
   production: false,
   firebase: {
-    apiKey: "AIzaSyCTqzghBSlEV5TL78MFXfs_aSWeoyT1s_w",
-    authDomain: "reunifal-cfe6c.firebaseapp.com",
-    databaseURL: "https://reunifal-cfe6c.firebaseio.com",
-    projectId: "reunifal-cfe6c",
-    storageBucket: "reunifal-cfe6c.appspot.com",
-    messagingSenderId: "453131582961",
-    appId: "1:453131582961:web:ad0591bec3709ff8531850",
-    measurementId: "G-BE8DCX5B9M"
+    apiKey: "",
+    authDomain: "",
+    databaseURL: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: "",
+    appId: "",
+    measurementId: ""
   }
 };
